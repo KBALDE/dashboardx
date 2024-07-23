713a99b1fedf90262d3cdbc7fb3c1a9abd66f5a1
kbalde <kindi.balde@gmail.com>
2024-07-23 16:12:04 +0700

    dashboard page

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -87,11 +87,11 @@
  }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-   try {
+  //return Response.json({
+  //  message:
+  //    'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  //});
+  try {
      await client.sql`BEGIN`;
      await seedUsers();
      await seedCustomers();
