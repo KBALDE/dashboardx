9480fd2e10b8b47326328e24c17a78a17e53bd66
kbalde <kindi.balde@gmail.com>
2024-11-14 14:38:55 +0100

    new update 2

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,5 @@
 import CardWrapper from '@/app/ui/dashboard/cards';
-import { Card } from '@/app/ui/dashboard/cards';
+//import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
@@ -13,7 +13,7 @@ import {
 import {
     //fetchRevenue,
     //fetchLatestInvoices,
-    fetchCardData,
+    //fetchCardData,
 } from '@/app/lib/data';
 import { Suspense } from 'react';
 
